a5a3915bc177999d4a680f3675422ddc120a471f
Andrei Teodorescu <andreiteodorescu@Andreis-MacBook-Pro.local>
2024-10-05 19:02:55 +0300

    feedback updates, select2 js

--- src/js/prod/main.js
+++ src/js/prod/main.js
@@ -110,13 +110,6 @@ $(".hamburger").on("click", function () {
   $(".header").toggleClass("nav-open");
   $("body").toggleClass("nav-is-open");
 });
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 0) {
-    $(".navigation-background").addClass("nav-bg-out");
-  } else {
-    $(".navigation-background").removeClass("nav-bg-out");
-  }
-});
 "use strict";
 
 $(".search-console-bar-classic .search-console-bar-dropdown").last().addClass("search-console-bar-dropdown-last");
